3f6b8776715cc6ef2842e9f7a0524c653b7b46ee
czmarc1 <czmarc1@apps.tcnj.edu>
2014-02-27 19:14:25 -0500

    Create style.cs

---
+++ styles/style.cs
@@ -0,0 +1,48 @@
+#wrapper{
+    width:400px;
+    margin: 0 auto 0 auto;
+    border: 1px solid #777;
+    
+}
+
+header.page{
+    margin: 10px;
+    padding: 10px;
+    border: 1px solid #000;
+    text-align: center;
+
+}
+
+header{
+    margin: 10px;
+    padding: 10px;
+}
+nav{
+    text-align: center;
+}
+
+nav li{
+    display: inline;
+    padding-right:20px;
+}
+article,aside{
+    float:left;
+}
+aside{
+    padding-left:200px;
+}
+footer{
+    border: 1px solid #000;
+    text-align: center;
+
+    clear:left;
+}
+
+aside li{
+    list-style-type:none;
+}
+
+a:link{color:#777}
+a:hover{color:#aaa}
+a:visited{color:#660000}
+a:active{color:#999900}
